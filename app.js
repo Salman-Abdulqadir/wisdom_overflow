ed272c981da97f932ec9bbe60b2dadbe268bfd59
salman-abdulqadir <salmanabdulkadir82@gmail.com>
2022-12-08 21:03:49 +0400

    basic functionalities finished

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 const api_url = "https://api.adviceslip.com/advice";
 
+//colors of the card
+const colors = [
+  "#dcf5dc",
+  "#f5f2dc",
+  "#f5e4dc",
+  "#dcf5ee",
+  "#dfdcf5",
+  "#f5dcf5",
+];
+let color_index = 0;
+//elements
+const button = document.querySelector(".green-btn");
+const card = document.querySelector(".card");
+
+//event listeners
+button.addEventListener("click", () => {
+  show_advice();
+  if(color_index > 5)
+    color_index = 0;
+  card.style.backgroundColor = colors[color_index];
+  color_index++;
+});
+
 // function that fetches data from the api
 const fetch_advice = async () => {
   try {
@@ -12,9 +35,7 @@ const fetch_advice = async () => {
 };
 
 const show_advice = async () => {
-    const card = document.querySelector("#advice");
-    const advice = await fetch_advice();
-
-    card.innerText = advice;
-}
-
+  const card = document.querySelector("#advice");
+  const advice = await fetch_advice();
+  card.innerText = advice;
+};
